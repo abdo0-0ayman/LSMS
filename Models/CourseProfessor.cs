7ef3f084f34879a699f3bafcf52de878715c467b
“AbdoSaad22” <abdosaad5513@gmail.com>
2023-12-03 20:58:03 +0200

    adding models

---
+++ Models/CourseProfessor.cs
@@ -0,0 +1,10 @@
+﻿namespace LSMS.Models
+{
+    public class CourseProfessor
+    {
+        public string CourseId { get; set; }
+        public Course Courses { get; set; }
+        public int ProfessorId { get; set; }
+        public Professor Professors { get; set; }
+    }
+}
---
+++ Models/CourseTeachingAssistant.cs
@@ -0,0 +1,10 @@
+﻿namespace LSMS.Models
+{
+    public class CourseTeachingAssistant
+    {
+        public string CourseId { get; set; }
+        public Course Courses { get; set; }
+        public int teachingAssistantId { get; set; }
+        public TeachingAssistant TeachingAssistants { get; set; }
+    }
+}
