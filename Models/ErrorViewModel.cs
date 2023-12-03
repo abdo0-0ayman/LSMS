752afef23b574c10af3ce2adcefca7a890e0386b
abdo0-0ayman <abdoayman9@gmail.com>
2023-12-03 19:59:15 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LSMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
