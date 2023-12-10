8930be9fd762441901c34e1c5c6204d1b34f0a98
Abdo-hany48 <abdelrhmanhany200348@gmail.com>
2023-12-10 12:50:35 +0200

    all basic controllers

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using LSMS.data_access;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LSMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Admins.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using LSMS.data_access;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LSMS.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data=_context.Students.ToList();
+            return View(data);
+        }
+    }
+}
